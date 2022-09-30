3510c731cbb66a3b3c8989d21aa678c0a37c7176
xaiur <syabilkahfialfatih@gmail.com>
2022-09-30 14:27:08 +0800

    Section: 2. GraphQL Basics Schemas and Queries - Chapter: 11. Operation Arguments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,22 +1,52 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 const server = new GraphQLServer({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      id: ID
-      name: String
-      age: Int
-      employed: Boolean
-      gpa: Float
-    }
-  `,
+  typeDefs: /* dalam GraphQL apabila field diisi tanda seru (Exclamation mark !) tidak boleh bernilai null */ `
+      type Query {
+        greeting(name: String, position: String): String!
+        add(a: Float, b: Float): Float
+        me: User! 
+        post: Post!
+      }
+
+      type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+      }
+
+      type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+      }
+    `,
   resolvers: {
     Query: {
-      id: () => 234214380732865895,
-      name: () => 'Alicia Visenya',
-      age: () => 30,
-      employed: () => true,
-      gpa: () => 22 / 7,
+      greeting(parent, args, ctx, info) {
+        if (args.name && args.position) {
+          return `Hello, ${args.name}. You are my favorite ${args.position}!`;
+        } else {
+          return 'Hello!';
+        }
+      },
+      add(parent, args, ctx, info) {
+        return args.a + args.b;
+      },
+      me: () => ({
+        id: 234214380732865895,
+        name: 'Alicia Visenya',
+        age: 30,
+        email: 'visenya@xaiur.com',
+      }),
+      post: () => ({
+        id: 343435455465767786,
+        title: 'GraphQL 101',
+        body: '',
+        published: false,
+      }),
     },
   },
 });
@@ -33,16 +63,38 @@ server.start(() => {
 //   schema: {
 //     typeDefs: /* GraphQL */ `
 //       type Query {
-//         hello: String
-//         name: String
-//         email: String
+//         me: User!
+//         post: Post!
+//       }
+
+//       type User {
+//         id: ID!
+//         name: String!
+//         email: String!
+//         age: Int
+//       }
+
+//       type Post {
+//         id: ID!
+//         title: String!
+//         body: String!
+//         published: Boolean!
 //       }
 //     `,
 //     resolvers: {
 //       Query: {
-//         hello: () => 'Hello from Yoga!',
-//         name: () => 'Hermawan Rinnaldi',
-//         email: () => 'ceo@xaiur.com',
+//         me: () => ({
+//           id: 234214380732865895,
+//           name: 'Alicia Visenya',
+//           age: 30,
+//           email: 'visenya@xaiur.com',
+//         }),
+//         post: () => ({
+//           id: 343435455465767786,
+//           title: 'GraphQL 101',
+//           body: '',
+//           published: false,
+//         }),
 //       },
 //     },
 //   },
